21c6a8249a158ea512048f6052f5b19f3259b841
Anhad Sodhi <anhadsodhi2005@gmail.com>
2025-11-12 17:14:22 -0800

    frontend-react/src/config/index.ts: Updated environment variables (added backend-rooms server endpoint)

--- frontend-react/src/config/index.ts
+++ frontend-react/src/config/index.ts
@@ -1,5 +1,6 @@
 const Config = {
-  API_BASE_URL: `https://anhadsodhi--isa-ai-server-fastapi-app.modal.run/`,
+  AUTH_BASE_URL: import.meta.env.VITE_AUTH_BASE_URL || 'http://localhost:8000',
+  ROOMS_BASE_URL: import.meta.env.VITE_ROOMS_BASE_URL || 'http://localhost:3000/API/v1',
 };
 
 export default Config;
